4ca5aaf97e0b7ddd9f95048785a5f2c4fa565a11
matohoundev <matohoundev@gmail.com>
2022-10-07 14:34:42 +0200

    add test newBill

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -2,17 +2,144 @@
  * @jest-environment jsdom
  */
 
-import { screen } from "@testing-library/dom"
-import NewBillUI from "../views/NewBillUI.js"
-import NewBill from "../containers/NewBill.js"
-
+import { screen, fireEvent } from "@testing-library/dom";
+import NewBill from "../containers/NewBill.js";
+import { ROUTES, ROUTES_PATH } from "../constants/routes";
+import { localStorageMock } from "../__mocks__/localStorage.js";
+import store from "../__mocks__/store.js";
+import router from "../app/Router.js";
 
 describe("Given I am connected as an employee", () => {
-  describe("When I am on NewBill Page", () => {
-    test("Then ...", () => {
-      const html = NewBillUI()
-      document.body.innerHTML = html
-      //to-do write assertion
-    })
-  })
-})
+  const onNavigate = (pathname) => {
+    document.body.innerHTML = ROUTES({ pathname });
+  };
+
+  beforeEach(() => {
+    document.body.innerHTML = "";
+    Object.defineProperty(window, "localStorage", { value: localStorageMock });
+    window.localStorage.setItem(
+      "user",
+      JSON.stringify({
+        type: "Employee",
+      })
+    );
+    const root = document.createElement("div");
+    root.setAttribute("id", "root");
+    document.body.append(root);
+    router();
+    onNavigate("#employee/bill/new");
+    jest.clearAllMocks();
+  });
+
+  describe("When I am on new bill page", () => {
+    test("Then the bill appears", () => {
+      document.body.innerHTML = "";
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.NewBill);
+
+      expect(screen.getByTestId("form-new-bill")).toBeTruthy();
+    });
+  });
+
+  // test upload file
+  describe("When i upload a new file", () => {
+    test("Then calls handleChangeFile and store for create a new bills", () => {
+      const spyStore = jest.spyOn(store, "bills");
+
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store,
+        localStorage: window.localStorage,
+      });
+
+      const fileInput = screen.getByTestId("file");
+      const handleChangeFile = jest.fn(newBill.handleChangeFile);
+
+      fileInput.addEventListener("change", handleChangeFile);
+
+      fireEvent.change(fileInput, {
+        target: {
+          files: [new File(["test.png"], "test.png", { type: "image/png" })],
+        },
+      });
+
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(spyStore).toHaveBeenCalled();
+    });
+    test("Then if the file is not an image", () => {
+      const spyStore = jest.spyOn(store, "bills");
+
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store,
+        localStorage: window.localStorage,
+      });
+
+      const fileInput = screen.getByTestId("file");
+      const handleChangeFile = jest.fn(newBill.handleChangeFile);
+
+      fileInput.addEventListener("change", handleChangeFile);
+
+      fireEvent.change(fileInput, {
+        target: {
+          files: [
+            new File(["test.pdf"], "test.pdf", { type: "application/pdf" }),
+          ],
+        },
+      });
+
+      expect(handleChangeFile).toHaveBeenCalled();
+      expect(spyStore).not.toHaveBeenCalled();
+      expect(fileInput.value).toEqual("");
+    });
+  });
+
+  // test d'intégration POST
+  describe("When I am on NewBill Page and I submit a new bill", () => {
+    test("Then a post request should be sent", () => {
+      Object.defineProperty(window, "localStorage", {
+        value: localStorageMock,
+      });
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          type: "Employee",
+        })
+      );
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+      router();
+      window.onNavigate(ROUTES_PATH.Bills);
+      const newBill = new NewBill({
+        document,
+        onNavigate,
+        store,
+        localStorage: window.localStorage,
+      });
+
+      const formNewBill = screen.getByTestId("form-new-bill");
+      const handleSubmit = jest.fn(newBill.handleSubmit);
+
+      formNewBill.addEventListener("submit", handleSubmit);
+
+      fireEvent.submit(formNewBill);
+
+      expect(handleSubmit).toHaveBeenCalled();
+    });
+  });
+});
